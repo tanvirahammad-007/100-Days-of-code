1e617a1178d0c0f3a0f3cc193975b85c3d714a00
Tanvir Ahammad <tanvirahammad890@gmail.com>
2025-07-04 00:10:05 +0600

    Add files via upload
    First day completed successfully!

---
+++ Day-1.py
@@ -0,0 +1,7 @@
+print("Hello" + " " + "There!")
+
+print("Hello" +" " + input("What's your name?") + "!")
+
+username = input("What's your name?")
+lenth = len(username)
+print(lenth)
---
+++ Day-1F.py
@@ -0,0 +1,4 @@
+print("Welcome to Band name generator!")
+city = input("Enter your city name :\n")
+member = input("How many member do you have in your band?\n")
+print("Your band name could be :" + city + " " + member)
